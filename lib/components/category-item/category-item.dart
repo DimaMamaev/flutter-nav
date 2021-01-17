e4cf1164f76cdecceea67cb071cb82c435cb5a81
DimaMamaev <dmamaev@my.com>
2021-01-17 14:24:33 +0200

    added new route to meals page

--- lib/components/category-item/category-item.dart
+++ lib/components/category-item/category-item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:menuApp/screens/meals/meals.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,21 +7,36 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem({this.title, this.color});
 
+  void onRouteHandler(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return MealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
+    return InkWell(
+      onTap: () => onRouteHandler(context),
+      splashColor: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [color.withOpacity(0.65), color],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.circular(10)),
       ),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [color.withOpacity(0.65), color],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(10)),
     );
   }
 }
---
+++ lib/screens/meals/meals.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: Center(
+        child: Text('Meals Page!'),
+      ),
+    );
+  }
+}
